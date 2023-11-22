74ff4f415d9a624144c76b9039a9c2b468a2ebbf
shanu-user <shanunandanwar12@gmail.com>
2023-11-22 10:07:12 -0800

    Solving errors after merging part 2

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose'
 import Stripe from 'stripe'
 import axios from 'axios'
-import {v4 as uuid} from 'uuid'
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -61,39 +59,3 @@ export const createPayment = async (req, res) =>{
     // }).then((result) => res.status(200).json(result))
     // .catch((err) => console.log(err))
 }
-=======
-import express from 'express'
-import stripePackage from 'stripe'
-import { v4 as uuidv4} from 'uuid'
-
-const stripe = stripePackage("pk_live_51OCHISSGM9rJz5UgnNk9jJkrb1EuBcsziHY4ROlfSeIJWIsPFYKYyyLPwHAwNVC47u0Vv4cBPkpwlMseuIpGH2k800fA4dXdMA")
-const idempotencyKey = uuidv4()
-
-export const createOrders = async (req, res) =>{
-    stripe.products.create({
-        name: 'Free Plan',
-        description: '1 question/day free-of-cost'
-    }).then(product => {
-        stripe.prices.create({
-            unit_amount: 0,
-            currency: 'inr',
-            recurring: {
-                interval: 'month'
-            },
-            product: idempotencyKey
-        }).then((result) => res.status(200).json(result))
-        .catch(err => console.log(err))
-    })
-    
-    stripe.products.create({
-        name: 'Silver Plan',
-        description: '5 questions/day costs 100 rupees/month'
-    })
-    
-    stripe.products.create({
-        name: 'Gold Plan',
-        description: 'Unlimited questions priced at 1000 rupees/month'
-    })
-
-}
->>>>>>> cfa06a7437310d3261df96e4173bdc6cc30825c0
--- routes/Payment.js
+++ routes/Payment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express'
 import { createPayment } from '../controllers/Payment.js'
 
@@ -7,14 +6,3 @@ const router = express.Router()
 router.post('/create-checkout-session', createPayment)
 
 export default router  
-=======
-import express from 'express'
-import { createOrders } from '../controllers/Payment.js' 
-
-const router = express.Router()
-
-router.get('/create', createOrders )
-
-
-export default router
->>>>>>> cfa06a7437310d3261df96e4173bdc6cc30825c0
